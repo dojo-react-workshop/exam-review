64ddb32ab0e95f736a985f3bb6ab3c37dd18c2e9
Charles Mead <meadch@gmail.com>
2017-06-06 14:02:11 -0500

    search form and api call

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import UserForm from './UserForm';
 import UserList from './UserList';
 import RepoList from './RepoList';
+import axios from 'axios';
 
 class App extends Component {
+    state = {
+        users: []
+    }
+    handleSearchFormSubmit = (searchTerm) => {
+
+        return axios.get(`https://api.github.com/search/users?q=${searchTerm}`)
+            .then(({ data }) => {
+                const { items: users } = data;
+                if (users < 1) {
+                    this.setState({
+                        users: []
+                    })
+                    throw new Error('No results');
+                }
+                this.setState({
+                    users: users
+                });
+            });
+    }
     render() {
-        const { users, repos } = this.props;
+        const { repos } = this.props;
         return (
             <div className="App container">
                 <h1>Github App</h1>
-                <UserForm />
-                <UserList users={users} />
+                <UserForm onSubmit={this.handleSearchFormSubmit} />
+                <UserList users={this.state.users} />
                 <RepoList repos={repos} />
             </div>
         )
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { func } from 'prop-types';
 
-const UserForm = () => {
-    return (
-        <div className="row">
-            <div className="large-12 columns">
-                <label>User Search</label>
-                <input type="text" placeholder="Github username..." />
-                <button>Search</button>
+class UserForm extends Component {
+    state = {
+        searchTerm: '',
+        error: null
+    }
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    }
+    handleSubmit = (event) => {
+        this.props.onSubmit(this.state.searchTerm)
+            .then(() => {
+                this.setState({ searchTerm: '', error: null })
+            })
+            .catch(() => {
+                this.setState({
+                    error: 'Unrecognized username'
+                })
+            })
+    }
+    render() {
+        let potentialError = null;
+        if (this.state.error) {
+            potentialError = <p className="notification-box alert">{this.state.error}</p>
+        }
+        return (
+            <div className="row">
+                <div className="large-12 columns">
+                    {potentialError}
+                    <label>User Search</label>
+                    <input
+                        type="text"
+                        placeholder="Github username..."
+                        value={this.state.searchTerm}
+                        onChange={this.handleChange}
+                    />
+                    <button onClick={this.handleSubmit} disabled={this.state.searchTerm === ''}>Search</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+}
+
+UserForm.propTypes = {
+    onSubmit: func.isRequired
 }
 export default UserForm;
